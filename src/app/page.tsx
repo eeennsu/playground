6e0a1c5d50bce1590f41dde0271d4a6534d3ca33
eeennsu <diso592@naver.com>
2024-09-24 22:30:55 +0900

    feat

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-import { ResizePanel } from '@/features/resize-panel/resize-panel'
+// import { ResizePanel } from '@/features/resize-panel/resize-panel'
+import { VerticalTimelineExample } from '@/features/vertical-timeline/vertical-timeline-example'
 
 export default function Home() {
     return (
-        <main className='min-h-dvh bg-black text-white w-full flex'>
-            <ResizePanel />
+        <main className='min-h-dvh w-full flex'>
+            <VerticalTimelineExample />
         </main>
     )
 }
